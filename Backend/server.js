009cf2e28e545c80a3bae17127a1ae1f00a475e7
Talha Khurram <tk.shiekh4567@gmail.com>
2025-07-25 05:19:26 -0700

    Completed the project

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const { connectToMongo } = require("./mongoose");
 const express = require('express')
 const cors = require("cors");
-const app = express()
+const path = require("path"); 
+const app = express();
 const authRoutes = require("./routes/auth-route");
 const certRoutes = require("./routes/cert-route");
 const authMiddlewear = require("./middlewears/authMiddlewear");
@@ -12,10 +13,17 @@ connectToMongo().catch(err => console.log(err));
 const port = 3000;
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" })); // or even 20mb if needed
+app.use(express.json({ limit: "10mb" })); 
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/certificates", authMiddlewear, certRoutes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
